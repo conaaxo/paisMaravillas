66704fe22dfc616355afada21a2c38851da386fb
Alberto González <albertoglezba@gmail.com>
2023-02-14 16:22:35 -0600

    reparando el juego de cuidas vs destruyes xD

--- js/cuidas.js
+++ js/cuidas.js
@@ -6,49 +6,49 @@ var instructions = [
                     "Ordena el camino que se tiene que recorrer para que las frutas y verduras lleguen a tu plato.",
                     "Descubre lo que sucede cuando no se compran mascotas que no deben serlo.",
                     "Descubre lo que sucede si no liberamos a nuestras mascotas.",
-                    "Descubre lo que sucede si no producimos más basura.",
+                    "Descubre lo que sucede si no producimos mï¿½s basura.",
                     "Descubre lo que pasa con nuestro planeta cuando tratamos de no contaminar tanto.",
                     "Ordena el camino que se tiene que recorrer para que la carne llegue a tu plato y descubre lo que le sucede a la selva.",
                     "Ordena el camino para ver lo que sucede cuando compras mascotas que no deben serlo.",
                     "Descubre como afectamos el ambiente cuando liberamos a nuestras mascotas.",
-                    "Ordena el recorrido que hace la basura que producimos y que no separamos y observa lo que puede llegar de tu casa al océano.",
-                    "Descubre lo que le pasa al pastizal y a los demás ecosistemas cuando usas demasiado un automóvil."
+                    "Ordena el recorrido que hace la basura que producimos y que no separamos y observa lo que puede llegar de tu casa al ocï¿½ano.",
+                    "Descubre lo que le pasa al pastizal y a los demï¿½s ecosistemas cuando usas demasiado un automï¿½vil."
                    ];
 var lastText = [
-                "Menos destrucción de selvas y de otros ambientes",
+                "Menos destrucciï¿½n de selvas y de otros ambientes",
                 "Se mantiene el cosistema a salvo",
                 "No hay especies invasoras en el ecosistema y se mantiene saludable.",
                 "Se mantiene el ecosistema a salvo",
                 "Se mantiene la temperatura estable en el ecosistema",
-                "Eliminación de selva",
+                "Eliminaciï¿½n de selva",
                 "Se acaban los animales en el bosque",
-                "Introducción de especies invasoras y deterioro del ambiente",
-                "Contaminación de ambientes y muerte de plantas y animales",
+                "Introducciï¿½n de especies invasoras y deterioro del ambiente",
+                "Contaminaciï¿½n de ambientes y muerte de plantas y animales",
                 "Calentamiento global y cambio en las condiciones de los ecosistemas."
                ];
 var grandText = [
                   "Se necesita menos espacio para obtener alimentos vegetales como las frutas, verduras, granos y cereales que para obtener alimentos animales como la carne, y por lo tanto se destruyen menos los ecosistemas.",
-                  "Al no comprar animales silvestres contribuyes a que ya no se capturen más animales, logrando que sus poblaciones no se acaben y vivan en libertad.",
-                  "Al mantener a mis  mascotas fuera de lugares donde no pertenecen ayudo a conservar a los ecosistemas y así no se deterioran.",
-                  "Al no comprar productos que generan basura tenemos más posibilidades de separarla y reciclarla, ayudando a conservar los ecosistemas limpios y así nos mantenemos a salvo de la contaminación.",
-                  "Al sustituir el automóvil andando en bicicleta, caminando, usando transporte público, etc. Ayudas a disminuir la contaminación y el cambio climático.",
-                  "Si seguimos consumiendo demasiada carne, las vacas necesitarán más espacio, y los ranchos e instalaciones para la cría de animales se comerán las tierras donde viven especies de la selva afectando también a otros ecosistemas.",
-                  "Si compras animales que no son mascotas, contribuyes a que más animales silvestres sean atrapados y que sus poblaciones se acaben. Déjalos vivir en libertad.",
-                  "Los peces de los acuarios son muy bonitos pero al ser liberados en lagos y ríos, se convierten en un gran problema. Arrastran con ellos el hábitat, eliminando a las especies locales de peces y a otros animales y plántas acuáticas.",
-                  "Si no controlamos la producción de basura, contaminamos. La basura es dañina para muchas especies y para nosotros mismos. Los peces que nos comemos pueden estar contaminados con nuestra propia basura.",
-                  "Los pastizales no se pueden proteger contra los efectos del calentamiento global. Si seguimos contaminando el aire, México será un gran desierto."
+                  "Al no comprar animales silvestres contribuyes a que ya no se capturen mï¿½s animales, logrando que sus poblaciones no se acaben y vivan en libertad.",
+                  "Al mantener a mis  mascotas fuera de lugares donde no pertenecen ayudo a conservar a los ecosistemas y asï¿½ no se deterioran.",
+                  "Al no comprar productos que generan basura tenemos mï¿½s posibilidades de separarla y reciclarla, ayudando a conservar los ecosistemas limpios y asï¿½ nos mantenemos a salvo de la contaminaciï¿½n.",
+                  "Al sustituir el automï¿½vil andando en bicicleta, caminando, usando transporte pï¿½blico, etc. Ayudas a disminuir la contaminaciï¿½n y el cambio climï¿½tico.",
+                  "Si seguimos consumiendo demasiada carne, las vacas necesitarï¿½n mï¿½s espacio, y los ranchos e instalaciones para la crï¿½a de animales se comerï¿½n las tierras donde viven especies de la selva afectando tambiï¿½n a otros ecosistemas.",
+                  "Si compras animales que no son mascotas, contribuyes a que mï¿½s animales silvestres sean atrapados y que sus poblaciones se acaben. Dï¿½jalos vivir en libertad.",
+                  "Los peces de los acuarios son muy bonitos pero al ser liberados en lagos y rï¿½os, se convierten en un gran problema. Arrastran con ellos el hï¿½bitat, eliminando a las especies locales de peces y a otros animales y plï¿½ntas acuï¿½ticas.",
+                  "Si no controlamos la producciï¿½n de basura, contaminamos. La basura es daï¿½ina para muchas especies y para nosotros mismos. Los peces que nos comemos pueden estar contaminados con nuestra propia basura.",
+                  "Los pastizales no se pueden proteger contra los efectos del calentamiento global. Si seguimos contaminando el aire, Mï¿½xico serï¿½ un gran desierto."
                 ];
 var mensajes = [
-                ["La selva antes de ser reemplazada", "La selva se reemplaza por cultivos", "Los cultivos no destruyen tanto como el ganado", "Si los alimentos vienen de muy lejos los camiones contaminan", "Los alimentos de los mercados vienen de lugares más cercanos que los de los supermercados", "Consumo muchos productos vegetales y menos productos animales"],
+                ["La selva antes de ser reemplazada", "La selva se reemplaza por cultivos", "Los cultivos no destruyen tanto como el ganado", "Si los alimentos vienen de muy lejos los camiones contaminan", "Los alimentos de los mercados vienen de lugares mï¿½s cercanos que los de los supermercados", "Consumo muchos productos vegetales y menos productos animales"],
                 ["Anmales que viven en el bosque", "Captura de polluelos en sus nidos", "Muchos polluelos mueren en el viaje a la ciudad", "Compra de animales como perros, gatos, etc. Que si son mascotas", "Como no hay quien los compre, ya no son capturados", "El bosque vuelve a la normalidad sin captura de aves"],
-                ["Compra de animales en tiendas de mascotas", "Peces en pequeñas peceras", "El pez creció demasiado, ya no cabe en la pecera", "Compro una pecera más grande y conservo a mi mascota", "Vive conmigo en su pecera hasta que muere", "Se conserva el ecosistema y las especies locales están a salvo"],
-                ["Productos que no generan basura", "Basura de casas, escuelas y edificios es separada", "Recolecta de basura en las ciudades", "Tiraderos de basura orgánica con centros de reciclaje", "Ríos y arroyos limpios que llegan hasta el mar", "Se mantiene el mar limpio para que puedan vivir las especies"],
-                ["Voy caminando a lugares cercanos", "Uso bicicleta para ir a la tienda", "Uso el transporte público", "Disminuyen los gases de las ciudades gracias a uso de otros medios de transporte", "Se disminuye el calentamiento de la tierra", "La temperatura del ecosistema se mantiene estable"],
-                ["En la selva vive una gran diversidad de plantas y animales", "La selva se destruye para criar vacas", "Las vacas no dejan alimento para otras especies", "La carne necesita ser transportada de muy lejos en camiones que contaminan", "La gente consume en supermercados y carnicerías", "Compras hamburguesas y ¡Llegan a tu plato! ¿Qué le pasó a la selva?"],
-                ["Las aves son libres en los bosques", "Los polluelos son capturados en sus nidos", "Al ser transportados, muchos animales se mueren en el camino", "Cuando compras un ave provocas que se capturen más", "Las jaulas son cárceles para las aves", "Sin aves un bosque no es saludable"],
-                ["Compra de animales en tiendas de mascotas", "Peces en pequeñas peceras", "El pez creció demasiado, ya no cabe en la pecera", "Liberas a los peces en donde no pertenecen", "Los peces del acuario se comen el alimento y a otros peces y transmiten enfermedades", "Las especies locales desaparecen"],
-                ["Compras todo tipo de productos con los empaques y envolturas incluidos los 'Chatarras'", "Tiras a la basura todo lo que sobra de tu consumo sin separarlo", "Los camiones que recolectan tu basura la juntan con la de miles de personas", "Toda la basura va a parar a tiraderos", "La basura que tiras en la calle termina en barrancas y ríos", "La basura llega a los océanos y las especies se mueren ¡Las ballenas no comen empaques! ¿O sí?"],
-                ["Te transportas constantemente en coche ¡Incluso para ir a la esquina!", "Muchos coches causan tránsito en las grandes ciudades", "Muchos coches circulando y embotellamientos, emiten gases que contaminan el ambiente y el aire", "La contaminación del aire es parte de las causas del calentamiento global", "Sin agua no se mantiene la vida", "Las plantas y animales de pastizales son afectadas por el calentamiento"]
+                ["Compra de animales en tiendas de mascotas", "Peces en pequeï¿½as peceras", "El pez creciï¿½ demasiado, ya no cabe en la pecera", "Compro una pecera mï¿½s grande y conservo a mi mascota", "Vive conmigo en su pecera hasta que muere", "Se conserva el ecosistema y las especies locales estï¿½n a salvo"],
+                ["Productos que no generan basura", "Basura de casas, escuelas y edificios es separada", "Recolecta de basura en las ciudades", "Tiraderos de basura orgï¿½nica con centros de reciclaje", "Rï¿½os y arroyos limpios que llegan hasta el mar", "Se mantiene el mar limpio para que puedan vivir las especies"],
+                ["Voy caminando a lugares cercanos", "Uso bicicleta para ir a la tienda", "Uso el transporte pï¿½blico", "Disminuyen los gases de las ciudades gracias a uso de otros medios de transporte", "Se disminuye el calentamiento de la tierra", "La temperatura del ecosistema se mantiene estable"],
+                ["En la selva vive una gran diversidad de plantas y animales", "La selva se destruye para criar vacas", "Las vacas no dejan alimento para otras especies", "La carne necesita ser transportada de muy lejos en camiones que contaminan", "La gente consume en supermercados y carnicerï¿½as", "Compras hamburguesas y ï¿½Llegan a tu plato! ï¿½Quï¿½ le pasï¿½ a la selva?"],
+                ["Las aves son libres en los bosques", "Los polluelos son capturados en sus nidos", "Al ser transportados, muchos animales se mueren en el camino", "Cuando compras un ave provocas que se capturen mï¿½s", "Las jaulas son cï¿½rceles para las aves", "Sin aves un bosque no es saludable"],
+                ["Compra de animales en tiendas de mascotas", "Peces en pequeï¿½as peceras", "El pez creciï¿½ demasiado, ya no cabe en la pecera", "Liberas a los peces en donde no pertenecen", "Los peces del acuario se comen el alimento y a otros peces y transmiten enfermedades", "Las especies locales desaparecen"],
+                ["Compras todo tipo de productos con los empaques y envolturas incluidos los 'Chatarras'", "Tiras a la basura todo lo que sobra de tu consumo sin separarlo", "Los camiones que recolectan tu basura la juntan con la de miles de personas", "Toda la basura va a parar a tiraderos", "La basura que tiras en la calle termina en barrancas y rï¿½os", "La basura llega a los ocï¿½anos y las especies se mueren ï¿½Las ballenas no comen empaques! ï¿½O sï¿½?"],
+                ["Te transportas constantemente en coche ï¿½Incluso para ir a la esquina!", "Muchos coches causan trï¿½nsito en las grandes ciudades", "Muchos coches circulando y embotellamientos, emiten gases que contaminan el ambiente y el aire", "La contaminaciï¿½n del aire es parte de las causas del calentamiento global", "Sin agua no se mantiene la vida", "Las plantas y animales de pastizales son afectadas por el calentamiento"]
                ];
 var copy = "";
 $(document).ready(function(){
@@ -131,7 +131,7 @@ function endActivity() {
   });
 }
 
-function resetAct() {
+function resetAct() {
   $("#iniActividad, #instActividad").show();
   $("#finActividad, #grandText, #resActividad").hide();
 }
